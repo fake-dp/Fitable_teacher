494eed4b18059d99b11c09357ae842451663c5f3
fake-dp <fakecoko92@gmail.com>
2023-10-05 12:52:16 +0900

    강사 첫 커밋

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import { View,Text } from 'react-native';
+
+function App(props) {
+  return (
+    <View>
+      <Text>ddd</Text>
+      <Text>ddd</Text>
+      <Text>ddd</Text>
+      <Text>ddd</Text>
+      <Text>ddd</Text>
+      <Text>ddd</Text>
+      <Text>ddd</Text>
+      <Text>ddd</Text>
+      <Text>ddd</Text>
+      <Text>ddd</Text>
+      <Text>ddd</Text>
+      <Text>ddd</Text>
+    </View>
+  );
+}
+
+export default App;
---
+++ ios/fitableteacher/fitableteacher/fitableteacher/fitableteacherApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitableteacherApp.swift
+//  fitableteacher
+//
+//  Created by notegg_005 on 2023/10/05.
+//
+
+import SwiftUI
+
+@main
+struct fitableteacherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
